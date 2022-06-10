57a2ce51311851c283b2ad51d3ed33a612bbeb66
Aleksandrs Stier <aleks.stier@icloud.com>
2022-06-11 00:38:57 +0200

    fix: wrong id printed if _NET_ACTIVE_WINDOW is set to None

--- xfocusnotify.c
+++ xfocusnotify.c
@@ -15,11 +15,12 @@ int main() {
     while (!XNextEvent(d, &e)) {
         if (e.xproperty.atom != net_active_window_atom)
             continue;
+        unsigned long n;
         unsigned char *net_active_window;
         XGetWindowProperty(d, root, net_active_window_atom,
             0, sizeof(Window), False, XA_WINDOW, &(Atom) {0}, &(int) {0},
-            &(unsigned long) {0}, &(unsigned long) {0}, &net_active_window);
-        if (net_active_window) {
+            &n, &(unsigned long) {0}, &net_active_window);
+        if (n && net_active_window) {
             printf("0x%lx\n", *(Window *) net_active_window);
             XFree(net_active_window);
         }
